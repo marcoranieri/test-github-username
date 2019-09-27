98dffb8fc3c75102ddbc8fc68fc2a8fb01c2f42c
Marco Ranieri <mr.marcoranieri@gmail.com>
2019-09-27 17:54:15 +0100

    FETCH API -> added n (as parameter) to choose how many TOP languages need to be returned

--- app/services/fetch_github_api.rb
+++ app/services/fetch_github_api.rb
@@ -11,13 +11,13 @@ class FetchGithubApi
     @repos = []
   end
 
-  def fetch_repos_languages
+  def fetch_top_n_repos_languages(n=3)
     result = Hash.new(0)
     @repos.each do |repo|
       result[repo["language"]] += 1 unless repo["language"].nil?
     end
 
-    result
+    result.sort_by {|a,b| -b}.first(n).to_h
   end
 
   def fetch_user_avatar_and_repos_url
@@ -32,8 +32,12 @@ class FetchGithubApi
 end
 
 # arthur = FetchGithubApi.new("   ar th ur-li ttm    ")
-# p arthur.fetch_repos_languages
-# p arthur.fetch_user_avatar_and_repos_url
 
+# p arthur.fetch_top_n_repos_languages
 # {"Ruby"=>18, "PHP"=>1, "JavaScript"=>1, "HTML"=>4, "CSS"=>1}
-# {"avatar"=>"https://avatars1.githubusercontent.com/u/16685604?v=4", "repos_url"=>"https://api.github.com/users/arthur-littm/repos"}
+
+# p arthur.fetch_user_avatar_and_repos_url
+# {
+#   "avatar"=>"https://avatars1.githubusercontent.com/u/16685604?v=4",
+#   "repos_url"=>"https://api.github.com/users/arthur-littm/repos"
+# }
